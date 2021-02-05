949d97a8a92343829af7226760b6bd5a7493975e
unclezs <1585503310@qq.com>
2021-02-05 23:58:19 +0800

    [210205] 更新文章

---
+++ js/post.js
@@ -0,0 +1,18 @@
+/*文章 */
+// const titleStyle = function (cssSelector) {
+//   const $title = $(cssSelector);
+//   $title.each(function () {
+//     const $this = $(this);
+//     text = $this.text();
+//     $this.text("");
+//     const title = `<span class="prefix"></span><span class="content">${text}</span><span class="suffix"></span>`;
+//     $this.prepend(title);
+//   });
+// };
+// /*页面载入完成后*/
+// !(function (e, t, a) {
+//   //标题
+//   // titleStyle("#nice h1");
+//   titleStyle("#nice h2");
+//   titleStyle("#nice h3");
+// })(window, document);
